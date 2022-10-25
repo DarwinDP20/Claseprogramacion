e3e756478068383d66b348dead61d4d537bb92c1
DarwinDP20 <dpadillatamay@gmail.com>
2022-10-25 15:09:29 -0500

    Contenido

---
+++ src/Contenido.java
@@ -0,0 +1,23 @@
+public class Clase {
+    public static void main(String[] args) {
+        System.out.println("La Programación da vida al hardware.");
+        System.out.println("Importancia de la programación");
+        System.out.println("Lenguajes de programación");
+        System.out.println("Entornos de programación");
+        System.out.println("Metodologías de programación");
+        System.out.println("La Programación da vida al hardware");
+        System.out.println("Comunicar instrucciones a los dispositivos");
+        System.out.println("Software de sistema");
+        System.out.println("Software de aplicación");
+        System.out.println("Un programa se escribe en un lenguaje de programación");
+        System.out.println("Entorno de Desarrollo Integrado o IDE");
+        System.out.println("Las instrucciones que expresan un algoritmo");
+        System.out.println("Lógica de la programación");
+        System.out.println("PSeudocódigo ");
+        System.out.println("Diagramas de flujo ");
+        System.out.println("Ordenado");
+        System.out.println("Definido ");
+        System.out.println("Finito");
+        System.out.println("Programación es la acción de escribir instrucciones y crear programas");
+    }
+}
---
+++ src/Git.java
@@ -0,0 +1,11 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Se inicia con git init");
+        System.out.println("Se coloca el comado git hub init");
+        System.out.println("Luego se pone el comnado git add para preparar el codigo");
+        System.out.println("Se usa git status para ver el estado del codigo");
+        System.out.println("Luego de todo lo anteriro colocamos git brancj -M main");
+        System.out.println("Luego se coloca git remote add origin para poner el origen del repositorio y poder subir el archivo a github");
+        System.out.println("Por ultimo ponemos push -u origin main para subir este archivo a github en el repositorio remoto");
+    }
+}
